9cadde62637ff93499647b39d5cf59fe55691661
LucaSorann0 <118919272+LucaSorann0@users.noreply.github.com>
2022-12-23 09:32:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+float f (float arg) {
+return arg*arg*cos(arg)+1;
+} 
+
 int main() {
+   float a=0, b=0, x=0, err=0;
+while (f(a)*f(b)>=0) {
+cout << "inserire estremi" << endl;
+cin >> a >> b;
+}
+do {
+x=(a+b)/2;
+if (f(x) != 0) {
+if (f(x)*f(b)<0) {
+a=x;
+} else {
+b=x;
+}
+err = (b-a)/2;
+}
+}while ((err >= 1e-6)||(-err >= 1e-6));
+printf ("%.4f, %.4f", x, f(x));
    return 0;
 }
